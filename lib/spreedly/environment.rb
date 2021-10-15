a526d17cf12f2516f0a42cdc856306d40406a20e
irenliber <irr.morgo@gmail.com>
2021-10-15 21:54:25 +0300

    Update environment.rb
    added logs for debug

--- lib/spreedly/environment.rb
+++ lib/spreedly/environment.rb
@@ -159,6 +159,8 @@ module Spreedly
 
     def deliver_to_receiver(receiver_token, payment_method_token, receiver_options)
       body = deliver_to_receiver_body(payment_method_token, receiver_options)
+      p "DELIVER_TO_RECEIVER_BODY"
+      p body
       api_post(deliver_to_receiver_url(receiver_token), body)
     end
 
